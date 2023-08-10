6aa0485378f607ab139962a970d9af22cda5a6be
Pablo Ferro <pablo.ferro@dohack.tech>
2023-08-10 16:36:01 -0300

    Add build scripts and config

---
+++ app.config.js
@@ -0,0 +1,43 @@
+const _merge = require("lodash/merge");
+require("dotenv").config();
+
+const APP_VARIANTS_CONFIG = {
+  DEV: {
+    appNameSuffix: " (dev)",
+    androidPackageSuffix: ".dev",
+    iOSBundleIdentifierSuffix: ".dev",
+  },
+  QA: {
+    appNameSuffix: " (qa)",
+    androidPackageSuffix: ".qa",
+    iOSBundleIdentifierSuffix: ".qa",
+  },
+};
+
+module.exports = ({ config }) => {
+  const variantConfig =
+    process.env.MY_ENVIRONMENT === "dev"
+      ? APP_VARIANTS_CONFIG.DEV
+      : APP_VARIANTS_CONFIG[process.env.APP_VARIANT];
+
+  let newConfig = _merge(config, {
+    extra: {
+      env:
+        process.env.MY_ENVIRONMENT === "dev" ? "DEV" : process.env.APP_VARIANT,
+    },
+  });
+
+  if (variantConfig) {
+    newConfig = _merge(newConfig, {
+      name: `${newConfig.name}${variantConfig.appNameSuffix}`,
+      android: {
+        package: `${newConfig.android.package}${variantConfig.androidPackageSuffix}`,
+      },
+      ios: {
+        bundleIdentifier: `${newConfig.ios.bundleIdentifier}${variantConfig.iOSBundleIdentifierSuffix}`,
+      },
+    });
+  }
+
+  return newConfig;
+};
